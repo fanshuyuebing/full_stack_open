fb39980c6f29f6e863f9dbc36815739e7b19f940
anhaoyu <2529153354@qq.com>
2026-05-13 10:39:01 +0800

    的萨芬萨的

--- backend/models/note.js
+++ backend/models/note.js
@@ -4,9 +4,14 @@ const noteSchema = new mongoose.Schema({
   content: {
     type: String,
     required: true,
-    minlength: 5,
+    minlength: 5
   },
   important: Boolean,
+
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 noteSchema.set('toJSON', {
